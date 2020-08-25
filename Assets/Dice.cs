c6a0dd65cad66db69adefb198f1e3d7d0f389193
Koushik Roy <66838109+koushik16roy@users.noreply.github.com>
2020-08-25 22:03:22 +0530

    Add files via upload

---
+++ Assets/Dice.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Dice : MonoBehaviour
+{
+    public Text score;
+    public Text highscore;
+
+    public void Start()
+    {
+        highscore.text = PlayerPrefs.GetInt("HIGHSCORE",0).ToString(); 
+    }
+
+    public void RollDice()
+    {
+        int number = Random.Range(1, 7);
+        score.text = number.ToString();
+
+        if(number > PlayerPrefs.GetInt("HIGHSCORE",0))
+        {
+            PlayerPrefs.SetInt("HIGHSCORE", number);
+            highscore.text = number.ToString();
+        }
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        highscore.text = "0";
+    }
+}
